c8dbe7e7404ad2148020cd805ee83c023a5f2cab
namxle <namxle3010@gmail.com>
2023-08-02 17:24:02 +0700

    Update Intervar.py

--- Intervar.py
+++ Intervar.py
@@ -542,11 +542,11 @@ def check_annovar_result():
         if paras['skip_annovar'] != True:
             sys.exit()
     if inputft.lower() == 'avinput' :
-        cmd="perl "+paras['table_annovar']+" "+paras['inputfile']+" "+paras['database_locat']+" -buildver "+paras['buildver']+" -remove -out "+ paras['outfile']+" -protocol refGene,esp6500siv2_all,1000g2015aug_all,avsnp147,dbnsfp35a,clinvar_20200316,gnomad_genome,dbscsnv11,dbnsfp31a_interpro,rmsk,ensGene,knownGene  -operation  g,f,f,f,f,f,f,f,r,g,g   -nastring ."+annovar_options
+        cmd="perl "+paras['table_annovar']+" "+paras['inputfile']+" "+paras['database_locat']+" -buildver "+paras['buildver']+" -remove -out "+ paras['outfile']+" -protocol refGene,esp6500siv2_all,1000g2015aug_all,avsnp147,dbnsfp35a,clinvar_20200316,gnomad_genome,dbscsnv11,dbnsfp31a_interpro,rmsk,ensGene,knownGene -operation  g,f,f,f,f,f,f,f,f,r,g,g -nastring ."+annovar_options
         print("%s" %cmd)
         os.system(cmd)
     if inputft.lower() == 'vcf' :
-        cmd="perl "+paras['table_annovar']+" "+paras['inputfile']+".avinput "+paras['database_locat']+" -buildver "+paras['buildver']+" -remove -out "+ paras['outfile']+" -protocol refGene,esp6500siv2_all,1000g2015aug_all,avsnp147,dbnsfp35a,clinvar_20200316,gnomad_genome,dbscsnv11,dbnsfp31a_interpro,rmsk,ensGene,knownGene   -operation  g,f,f,f,f,f,f,f,r,g,g   -nastring ."+annovar_options
+        cmd="perl "+paras['table_annovar']+" "+paras['inputfile']+".avinput "+paras['database_locat']+" -buildver "+paras['buildver']+" -remove -out "+ paras['outfile']+" -protocol refGene,esp6500siv2_all,1000g2015aug_all,avsnp147,dbnsfp35a,clinvar_20200316,gnomad_genome,dbscsnv11,dbnsfp31a_interpro,rmsk,ensGene,knownGene -operation  g,f,f,f,f,f,f,f,f,r,g,g -nastring ."+annovar_options
         print("%s" %cmd)
         os.system(cmd)
         
